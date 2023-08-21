cb5eb2db80cc498cc97d41aef1df4767992f918b
kevinramos2 <keramosl@unal.edu.co>
2023-08-21 11:50:52 -0500

    clase Auto y metodo cantidadAsientos
    creación de la clase Auto y el primer método

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ class Asiento:
     def cambiarColor(self, color):
         if (color == 'amarillo' or color == 'verde' or color == 'rojo' or color == 'blanco' or color == 'negro'):
             self.color = color
+
+class Auto:
+    def init(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    cantidadCreados = 0
+
+    def cantidadAsientos(self):
+        totalAsientos = 0
+        for asiento in self.asiento:
+            if type(asiento) == Asiento:
+                totalAsientos += 1
+        return totalAsientos
+        
