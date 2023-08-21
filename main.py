d24a79c45454d652bbb5764d446a01a5a5b1e9c1
kevinramos2 <keramosl@unal.edu.co>
2023-08-21 12:36:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Asiento:
         self.registro = registro
       
     def cambiarColor(self, color):
-        if color in  ['rojo', ' verde', 'amarillo', 'negro', 'blanco']:
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
             self.color = color
 
 class Auto:
